981e3922ec0d553113d359b543207d7f2ee06055
turbodarbai-commits <turbodarbai@gmail.com>
2025-09-28 15:35:06 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
-import logo from "../static/images/turbo.png"
+import logo from "../../static/images/turbo.png"
 
 export default function Home() {
   return (
