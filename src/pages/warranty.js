be83fb2b5a3879a24b8ead09a191b8b32ab40e1e
turbodarbai-commits <turbodarbai@gmail.com>
2025-09-28 19:39:16 +0300

    hhh

---
+++ src/pages/warranty.js
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>PDF Viewer</title>
+</head>
+<body>
+  <h1>View PDF in Page</h1>
+  <iframe src="warranty.pdf" width="100%" height="600px"></iframe>
+</body>
+</html>
